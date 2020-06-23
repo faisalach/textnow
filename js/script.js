4fbcdc0674a8cc6719659e9491ef5c3a9d2a52c8
faisalach <faisalachramsyah@gmail.com>
2020-06-23 19:28:55 +0700

    v1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+	$('.newContact').addClass('d-none');
+	$('.addNewContact').on('click',function(e) {
+		e.preventDefault();
+		$('.contact').removeClass('active');
+		$('.withContact').addClass('d-none');
+		$('.newContact').removeClass('d-none');
+	})
+	$('.contact').on('click',function(e) {
+		$('.contact').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+		$('.newContact').addClass('d-none');
+		$('.withContact').removeClass('d-none');
+	})
+})
